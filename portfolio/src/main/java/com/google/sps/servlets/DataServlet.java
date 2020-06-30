307188f3433ed2ff0a225dadbb48527625f4b878
George Zheng <gzheng99@mit.edu>
2020-06-30 11:56:42 -0400

    added required imports for dataservlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,9 @@
 package com.google.sps.servlets;
 import com.google.gson.*;
 import com.google.sps.data.Comments;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
