c6679dc638e0a3b40061263654f1a4ade79ad0ac
George Zheng <gzheng99@mit.edu>
2020-07-07 13:08:11 -0400

    setup basic map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -41,6 +41,16 @@ async function deleteComments() {
 }
 
 /**
+ * Initializes a map.
+ */
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
+/**
  * Leads user to a selected project.
  */
 function chooseFeaturedProj() {
