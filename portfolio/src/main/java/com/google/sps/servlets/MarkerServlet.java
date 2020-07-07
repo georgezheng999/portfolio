46039ef4a1b1e4341fcaee5630b80a0ce5b28807
George Zheng <gzheng99@mit.edu>
2020-07-07 13:41:36 -0400

    fixed java syntax errors

--- portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MarkerServlet.java
@@ -30,8 +30,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
 
 /** Handles fetching and saving markers data. */
 @WebServlet("/markers")
