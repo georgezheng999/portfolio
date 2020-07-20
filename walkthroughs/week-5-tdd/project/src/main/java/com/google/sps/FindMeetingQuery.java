1d3a99ca803f6c09fe548dc22026a3f36f5a85b0
George Zheng <gzheng99@mit.edu>
2020-07-20 12:03:15 -0400

    remvoed optimization code to revert back to basic desired spec

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps;
 
 import java.util.Collection;
@@ -19,16 +5,28 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
 
 public final class FindMeetingQuery {
 
-  private static final TimeRange START_OF_DAY_TIME_RANGE = TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0);
+  // Returns whether or not any mandatory or optional attendees of this request have a conflict with the event.
+  // Setting considerOpt to true toggles the consideration of optional attendees
+  private boolean eventHasConflict(Event e, MeetingRequest request, boolean considerOpt) {
+    final Collection<String> mandAttendees = request.getAttendees();
+    final Collection<String> optAttendees = request.getOptionalAttendees();
+    for (final String eventAttendee : e.getAttendees()) {
+      if (mandAttendees.contains(eventAttendee)) {
+        return true;
+      }
+      if (considerOpt && optAttendees.contains(eventAttendee)) {
+        return true;
+      } 
+    }
+    return false;
+  }
 
-  public Collection<TimeRange> getOptimizedOpenTimes(List<TimeRange> conflictTimes, MeetingRequest request) {
-    conflictTimes.add(START_OF_DAY_TIME_RANGE);
+  // Given a list conflictTimes containing times that are not open and a request,
+  // returns a complementary list of open times that are at least as long as the requested duration
+  public Collection<TimeRange> getOpenTimes(List<TimeRange> conflictTimes, MeetingRequest request) {
     Collections.sort(conflictTimes, TimeRange.ORDER_BY_START);
     final Collection<TimeRange> queryResult = new ArrayList<>();
     final int requestDuration = (int) request.getDuration();
@@ -36,8 +34,10 @@ public final class FindMeetingQuery {
     for (int i = 1; i < conflictTimes.size(); i++) {
       final int durationDiff = conflictTimes.get(i).start() - maxEndingTimeSoFar; 
       if (durationDiff >= requestDuration) {
+        queryResult.add(TimeRange.fromStartDuration(conflictTimes.get(i - 1).end(), durationDiff));
         queryResult.add(TimeRange.fromStartDuration(maxEndingTimeSoFar, durationDiff));
       }
+      maxEndingTimeSoFar = max(maxEndingTimeSoFar, conflictTimes.get(i).end());
       maxEndingTimeSoFar = Math.max(maxEndingTimeSoFar, conflictTimes.get(i).end());
     }
     if (TimeRange.END_OF_DAY - maxEndingTimeSoFar >= requestDuration) {
@@ -46,50 +46,22 @@ public final class FindMeetingQuery {
     return queryResult;
   }
 
-  public Collection<TimeRange> queryOptimized(Collection<Event> events, MeetingRequest request) {
-    final Map<String, List<TimeRange>> optAttToConflicts = new HashMap<>();
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     List<TimeRange> mandConflictTimes = new ArrayList<>();
+    List<TimeRange> mandAndOptConflictTimes = new ArrayList<>();
+    mandConflictTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0)); 
+    mandAndOptConflictTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0)); 
     for (final Event e : events) {
-      for (final String eventAttendee : e.getAttendees()) {
-        if (request.getAttendees().contains(eventAttendee)) {
-          mandConflictTimes.add(e.getWhen());
-        }
-        if (request.getOptionalAttendees().contains(eventAttendee)) {
-          getOptionalAttendees.putIfAbsent(eventAttendee, new ArrayList<TimeRange>()).add(e.getWhen());
-        }
+      if (eventHasConflict(e, request, false)) {
+        mandConflictTimes.add(e.getWhen());
+      } 
+      if (eventHasConflict(e, request, true)) {
+        mandAndOptConflictTimes.add(e.getWhen());
       }
     }
-    final List<String> optAttendees = new ArrayList<>(request.getOptionalAttendees());
-    Collection<TimeRange> queryOptResult =  new ArrayList<>();
-    int maxOptAttendeesAttained = 0;
-    for (final List<String> optAttSubset : powerSet(optAttendees)) {
-      if (maxOptAttendeesAttained > optAttSubset.size()) {
-        continue;
-      }
-      List<TimeRange> conflictTimes = new ArrayList<>(mandConflictTimes);
-      for (final String optAttendee : optAttSubset) {
-        conflictTimes.addAll(optAttToConflicts.getOrDefault(optAttendee, new ArrayList<>()));
-      }
-      final Collection<TimeRange> sol = getOptimizedOpenTimes(conflictTimes, request);
-      if (!sol.isEmpty() && maxOptAttendeesAttained < optAttSubset.size()) {
-        maxOptAttendeesAttained = optAttSubset.size();
-        queryOptResult = sol;
-      }
-    }
-    final Collection<TimeRange> mandatoryUserTimes = getOptimizedOpenTimes(mandConflictTimes, request);
-    return (!mandatoryUserTimes.isEmpty() && !queryOptResult.isEmpty()) ? queryOptResult : mandatoryUserTimes; 
+    final Collection<TimeRange> mandatoryUserTimes = getOpenTimes(mandConflictTimes, request);
+    final Collection<TimeRange> mandAndOptUserTimes = getOpenTimes(mandAndOptConflictTimes, request);
+    return (mandAndOptUserTimes.isEmpty()) ? mandatoryUserTimes : mandAndOptUserTimes; 
+    return (mandAndOptUserTimes.isEmpty() && !request.getAttendees().isEmpty()) ? mandatoryUserTimes : mandAndOptUserTimes; 
   }
-
-  public List<List<String>> powerSet(List<String> optAttendees) {
-    List<List<String>> result = new ArrayList<>();
-    result.add(new ArrayList<String>());
-    for (final String attendee : optAttendees) {
-      for (int i = 0, n = result.size(); i < n; i++) {
-        result.add(new ArrayList<String>(result[i]));
-        result[i].add(optAttendees[i]);
-      }
-    }
-    return result;
-  }
-
 }
