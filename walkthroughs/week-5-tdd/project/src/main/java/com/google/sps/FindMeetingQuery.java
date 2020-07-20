d7f9754582414f403050f24b6de61eb36fcaca9b
George Zheng <georgezheng999@gmail.com>
2020-07-20 22:54:20 +0000

    now passes all test cases

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -27,6 +27,7 @@ public final class FindMeetingQuery {
   // Given a list conflictTimes containing times that are not open and a request,
   // returns a complementary list of open times that are at least as long as the requested duration
   public Collection<TimeRange> getOpenTimes(List<TimeRange> conflictTimes, MeetingRequest request) {
+    conflictTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0));
     Collections.sort(conflictTimes, TimeRange.ORDER_BY_START);
     final Collection<TimeRange> queryResult = new ArrayList<>();
     final int requestDuration = (int) request.getDuration();
@@ -34,10 +35,8 @@ public final class FindMeetingQuery {
     for (int i = 1; i < conflictTimes.size(); i++) {
       final int durationDiff = conflictTimes.get(i).start() - maxEndingTimeSoFar; 
       if (durationDiff >= requestDuration) {
-        queryResult.add(TimeRange.fromStartDuration(conflictTimes.get(i - 1).end(), durationDiff));
         queryResult.add(TimeRange.fromStartDuration(maxEndingTimeSoFar, durationDiff));
       }
-      maxEndingTimeSoFar = max(maxEndingTimeSoFar, conflictTimes.get(i).end());
       maxEndingTimeSoFar = Math.max(maxEndingTimeSoFar, conflictTimes.get(i).end());
     }
     if (TimeRange.END_OF_DAY - maxEndingTimeSoFar >= requestDuration) {
@@ -49,8 +48,6 @@ public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     List<TimeRange> mandConflictTimes = new ArrayList<>();
     List<TimeRange> mandAndOptConflictTimes = new ArrayList<>();
-    mandConflictTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0)); 
-    mandAndOptConflictTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0)); 
     for (final Event e : events) {
       if (eventHasConflict(e, request, false)) {
         mandConflictTimes.add(e.getWhen());
@@ -61,7 +58,6 @@ public final class FindMeetingQuery {
     }
     final Collection<TimeRange> mandatoryUserTimes = getOpenTimes(mandConflictTimes, request);
     final Collection<TimeRange> mandAndOptUserTimes = getOpenTimes(mandAndOptConflictTimes, request);
-    return (mandAndOptUserTimes.isEmpty()) ? mandatoryUserTimes : mandAndOptUserTimes; 
     return (mandAndOptUserTimes.isEmpty() && !request.getAttendees().isEmpty()) ? mandatoryUserTimes : mandAndOptUserTimes; 
   }
 }
